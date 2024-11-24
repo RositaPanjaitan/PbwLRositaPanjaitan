70e3aa1f330b4514d9e8f9b062370e31109f5384
RositaPanjaitan <Rositapanjaitan0101@gmail.com>
2024-11-24 21:14:00 +0700

    menggunakan bootstrap 5

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('about', function () {
+    return view('about');
+});
+
+Route::get('mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('profile', function () {
+    $nama = 'rosita';
+
+    return view('profile') ->with('nama', $nama);
 });
